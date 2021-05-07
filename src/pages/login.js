2370db1306cc3881e35fe7d29f127e3b0e577bbb
harik-6 <haribalaji32@gmail.com>
2021-05-07 18:53:03 +0530

    name changes

--- src/pages/login.js
+++ src/pages/login.js
@@ -103,10 +103,7 @@ const Login = () => {
         <Grid item lg={12} alignItems="center" container spacing={1}>
           <Grid item lg={7}>
             <div className={classes.txtcnt}>
-              <p className={classes.maintxt}>StreamWell</p>
-              <p className={classes.subtxt}>
-                For fastest and smooth streaming network.
-              </p>
+              <p className={classes.maintxt}>AndariTV</p>
             </div>
           </Grid>
           <Grid item lg={3}>
