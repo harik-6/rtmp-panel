5fa23cb72b9ba1fc914aec54db3a3a4f16194f2b
harik-6 <haribalaji32@gmail.com>
2021-05-07 18:53:45 +0530

    name changes

--- src/pages/main.js
+++ src/pages/main.js
@@ -89,7 +89,7 @@ const Main = () => {
     <div className={classes.appmain}>
       <Router>
         <div className={classes.sidenav}>
-          <p className={classes.appname}>StreamWell</p>
+          <p className={classes.appname}>AndariTV</p>
           <List className={classes.navlist} aria-label="navigation-list">
             <Link to="/home">
               <ListItem
