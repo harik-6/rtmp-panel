1061311462b3f3bacd17c319354dee1407288c5d
harik-6 <haribalaji32@gmail.com>
2022-07-02 20:53:43 +0530

    updated sever ui

--- src/pages/servers/server.s.js
+++ src/pages/servers/server.s.js
@@ -9,10 +9,10 @@ import TableContainer from "@mui/material/TableContainer";
 import { Button, Stack } from "@mui/material";
 import TableRow from "@mui/material/TableRow";
 //icons
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
-import BwEnabled from "@mui/icons-material/HourglassEmptyOutlined";
-import BwDisabled from "@mui/icons-material/HourglassDisabledOutlined";
+// import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+// import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
+// import BwEnabled from "@mui/icons-material/HourglassEmptyOutlined";
+// import BwDisabled from "@mui/icons-material/HourglassDisabledOutlined";
 import AddIcon from "@mui/icons-material/Add";
 import DeleteIcon from "@mui/icons-material/DeleteOutlined";
 
@@ -24,9 +24,9 @@ import CreateNewChannel from "../../components/Channel/Createchannel";
 import {
   getServers,
   rebootServer,
-  limitStatus,
-  startLimit,
-  stopLimit,
+  // limitStatus,
+  // startLimit,
+  // stopLimit,
   deleteServer,
 } from "../../service/server.service";
 import EditServer from "../../components/Server/EditServer";
@@ -44,10 +44,15 @@ const TableCellStyled = styled(TableCell)`
   padding: 12px 0;
 `;
 
+const TableHeaderText = styled.p`
+  font-weight: bold;
+`
+
 const TableContainerStyled = styled(TableContainer)`
   background-color: #ffffff;
   border-radius: 16px;
 `;
+let prevIp = "0.0.0.0.";
 
 const ServersS = ({ map }) => {
   // stat variabled
@@ -75,26 +80,26 @@ const ServersS = ({ map }) => {
     }, 4 * 1000);
   };
 
-  const _limitStatus = async (_sid, _status) => {
-    try {
-      await limitStatus(_sid, _status);
-      _loadAllServers();
-    } catch (_) {}
-  };
+  // const _limitStatus = async (_sid, _status) => {
+  //   try {
+  //     await limitStatus(_sid, _status);
+  //     _loadAllServers();
+  //   } catch (_) { }
+  // };
 
-  const _startLimit = async (_sname, bwIn, bwOut, _sid) => {
-    try {
-      await startLimit(_sname, bwIn, bwOut);
-      await _limitStatus(_sid, true);
-    } catch (_) {}
-  };
+  // const _startLimit = async (_sname, bwIn, bwOut, _sid) => {
+  //   try {
+  //     await startLimit(_sname, bwIn, bwOut);
+  //     await _limitStatus(_sid, true);
+  //   } catch (_) { }
+  // };
 
-  const _stopLimit = async (_sname, _sid) => {
-    try {
-      await stopLimit(_sname);
-      await _limitStatus(_sid, false);
-    } catch (_) {}
-  };
+  // const _stopLimit = async (_sname, _sid) => {
+  //   try {
+  //     await stopLimit(_sname);
+  //     await _limitStatus(_sid, false);
+  //   } catch (_) { }
+  // };
 
   const _editServer = async (_s) => {
     setActiveServer(_s);
@@ -109,8 +114,18 @@ const ServersS = ({ map }) => {
     _loadAllServers();
   };
 
+  const _canRenderIp = (ip) => {
+    if (ip === prevIp) {
+      return false;
+    }
+    prevIp = ip;
+    return true;
+  }
+
+
   useEffect(() => {
     _loadAllServers();
+    prevIp = "0.0.0.0.";
     // eslint-disable-next-line
   }, []);
 
@@ -121,6 +136,8 @@ const ServersS = ({ map }) => {
   if (servers.length === 0) {
     return <Preloader message={"Loading servers..."} />;
   }
+
+
 
   return (
     <ServersPage>
@@ -138,45 +155,60 @@ const ServersS = ({ map }) => {
       <TableContainerStyled>
         <Table aria-label="channel-list">
           <TableRow>
-            <TableCellStyled align="left">IP</TableCellStyled>
-            <TableCellStyled align="left">Domain</TableCellStyled>
+            <TableCellStyled style={{ fontWeight: 'bold' }} align="left">
+              <TableHeaderText>IP</TableHeaderText>
+            </TableCellStyled>
+            <TableCellStyled align="left">
+              <TableHeaderText>Domain</TableHeaderText>
+            </TableCellStyled>
             {/* <TableCellStyled align="left">Bandwidth IN</TableCellStyled>
             <TableCellStyled align="left">Bandwidth Out</TableCellStyled> */}
-            <TableCellStyled align="left">Limit</TableCellStyled>
-            <TableCellStyled align="left"># Channels</TableCellStyled>
-            <TableCellStyled align="left">BW Status</TableCellStyled>
-            <TableCellStyled align="left">Secured</TableCellStyled>
+            <TableCellStyled align="left">
+              <TableHeaderText>Limit</TableHeaderText>
+            </TableCellStyled>
+            <TableCellStyled align="left">
+              <TableHeaderText># Channels</TableHeaderText>
+            </TableCellStyled>
+            {/* <TableCellStyled align="left">BW Status</TableCellStyled> */}
+            {/* <TableCellStyled align="left">
+              <TableHeaderText>Secured</TableHeaderText>
+            </TableCellStyled> */}
             {/* <TableCellStyled align="left">Version</TableCellStyled> */}
-            <TableCellStyled align="left">Actions</TableCellStyled>
-            <TableCellStyled align="left">Delete</TableCellStyled>
+            <TableCellStyled align="left">
+              <TableHeaderText>Actions</TableHeaderText>
+            </TableCellStyled>
+            <TableCellStyled align="left">
+              <TableHeaderText>Delete</TableHeaderText>
+            </TableCellStyled>
           </TableRow>
           <TableBody>
             {servers.map((s, index) => {
               const _sname = s.domain;
               const _limit = s.limit;
               const _used = map[_sname] || "N/A";
+              const _canRedner = _canRenderIp(s.ip);
               return (
                 <TableRow key={s.id + " " + index}>
-                  <TableCellStyled align="left">{s.ip}</TableCellStyled>
+                  <TableCellStyled align="left">{_canRedner && s.ip}</TableCellStyled>
                   <TableCellStyled align="left">{_sname}</TableCellStyled>
                   {/* <TableCellStyled align="left">{s.bwIn}</TableCellStyled> */}
                   {/* <TableCellStyled align="left">{s.bwOut}</TableCellStyled> */}
                   <TableCellStyled align="left">{_limit}</TableCellStyled>
                   <TableCellStyled align="left">{_used}</TableCellStyled>
-                  <TableCellStyled align="left">
+                  {/* <TableCellStyled align="left">
                     {s.isBwEnabled ? (
                       <BwEnabled sx={{ color: "green" }} />
                     ) : (
                       <BwDisabled sx={{ color: "red" }} />
                     )}
-                  </TableCellStyled>
-                  <TableCellStyled align="left">
+                  </TableCellStyled> */}
+                  {/* <TableCellStyled align="left">
                     {s.isAuthEnabled ? (
                       <LockOutlinedIcon sx={{ color: "green" }} />
                     ) : (
                       <LockOpenOutlinedIcon sx={{ color: "red" }} />
                     )}
-                  </TableCellStyled>
+                  </TableCellStyled> */}
 
                   {/* <TableCellStyled align="left">{s.version}</TableCellStyled> */}
                   <TableCellStyled align="left">
@@ -203,7 +235,7 @@ const ServersS = ({ map }) => {
                       >
                         Edit
                       </Button>
-                      <Button
+                      {/* <Button
                         sx={{ textTransform: "none" }}
                         disableElevation
                         size="small"
@@ -220,8 +252,8 @@ const ServersS = ({ map }) => {
                         }
                       >
                         Start limit
-                      </Button>
-                      <Button
+                      </Button> */}
+                      {/* <Button
                         sx={{ textTransform: "none" }}
                         disableElevationsize="small"
                         variant="contained"
@@ -229,17 +261,19 @@ const ServersS = ({ map }) => {
                         onClick={() => _stopLimit(_sname, s.id, s.isBwLimited)}
                       >
                         Stop limit
-                      </Button>
-                      <Button
-                        sx={{ textTransform: "none" }}
-                        disableElevation
-                        size="small"
-                        variant="outlined"
-                        color="secondary"
-                        onClick={() => _rebootServer(_sname)}
-                      >
-                        Reboot
-                      </Button>
+                      </Button> */}
+                      {
+                        _canRedner && <Button
+                          sx={{ textTransform: "none" }}
+                          disableElevation
+                          size="small"
+                          variant="outlined"
+                          color="secondary"
+                          onClick={() => _rebootServer(_sname)}
+                        >
+                          Reboot
+                        </Button>
+                      }
                     </Stack>
                   </TableCellStyled>
                   <TableCellStyled color="primary" align="left">
@@ -279,11 +313,13 @@ const ServersS = ({ map }) => {
       <CreateNewChannel
         open={_opencreate}
         onClose={() => setOpencreate(false)}
-        callback={() => {}}
+        callback={() => { }}
         server={_server}
       />
     </ServersPage>
   );
 };
+
+
 
 export default ServersS;
